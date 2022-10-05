bbe2d0d0c709a9be6f76e8c97ce2bb598c0a59d8
developerronnie <developerronnie@yahoo.com>
2022-10-05 20:44:09 +0600

    code ready for upload

---
+++ 6.scope.js
@@ -0,0 +1,25 @@
+
+
+
+
+function add(first , second ){
+    
+    console.log(first)
+    const result = first + second ;
+    return result; 
+}
+
+const sum = add(11, 12);
+
+
+
+
+
+
+
+
+
+
+
+
+
